d30e1e2f40fc8b4baa61b8b87cfc4095dd97f65f
Dhruva Shaw <dhruvashaw@gmail.com>
2021-02-21 20:44:21 +0530

    update

--- puja/storage.py
+++
@@ -1,5 +0,0 @@
-from django.core.files.storage import FileSystemStorage
-from gdstorage.storage import GoogleDriveStorage
-
-class GoogleDriveStorageSystemPuja(GoogleDriveStorage):
-    is_secure = False
