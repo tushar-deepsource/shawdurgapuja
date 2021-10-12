18d82c59a19d6feeee5ba0559374726fc02a9e53
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-10-12 19:13:30 +0530

    update

--- main/templatetags/extra.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library() 
-
-@register.filter(name='give_correct_object')
-def give_correct_object(objects, slice_no):
-    object_list = [i for i in objects]
-    return object_list[slice_no-1]
